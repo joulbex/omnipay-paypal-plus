36344a980db7514b4a01b078e66645507600c8f2
Aleksander Meloch <aleksander.meloch@gmail.com>
2019-06-18 15:41:44 +0200

    Added billingAddress to UpdatePayment request.

--- src/Message/RestUpdatePaymentRequest.php
+++ src/Message/RestUpdatePaymentRequest.php
@@ -13,22 +13,45 @@ class RestUpdatePaymentRequest extends RestAuthorizeRequest
 {
     public function getData()
     {
+        $billingAddress = $this->getBillingAddress();
+
         $data = array(
             array(
                 'op' => 'replace',
                 'path' => '/transactions/0/amount',
                 'value' => array(
                     'total' => $this->getAmount(),
-                    'currency' => $this->getCurrency(),
-                    /*'details' => array(
-                        'subtotal' => 
-                        'shipping' => 
-                    )*/
+                    'currency' => $this->getCurrency()
+                )
+            ),
+            array(
+                'op' => 'add',
+                'path' => '/payer/payer_info',
+                'value' => array(
+                    'first_name' => $billingAddress['firstName'],
+                    'last_name' => $billingAddress['lastName'],
+                    'email' => $billingAddress['email'],
+                    'billing_address' => array(
+                        'line1' => $billingAddress['street'],
+                        'city' => $billingAddress['city'],
+                        'country_code' => $billingAddress['country'],
+                        'postal_code' => $billingAddress['zipCode']
+                    )
                 )
             )
         );
 
         return $data;
+    }
+
+    public function getBillingAddress()
+    {
+        return $this->getParameter('billingAddress');
+    }
+
+    public function setBillingAddress($value)
+    {
+        $this->setParameter('billingAddress', $value);
     }
     
     protected function getHttpMethod()
